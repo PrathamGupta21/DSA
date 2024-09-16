8d78ab17cf044102c1cc57e5f4d26bf9a30c5549
PRATHAM GUPTA <PRATHAM17GUPTA@GMAIL.COM>
2024-09-17 02:18:17 +0530

    array questions

---
+++ 1. Array/1-max-and-second-max.js
@@ -0,0 +1,43 @@
+// Ques 1 - Second Largest Number
+// Given an array Arr of size N, print second largest
+// distinct element from an array.
+
+// Input: [12, 35, 1, 10, 34, 1]  ----->>>>>  Output: 34
+// Input: [10, 5, 10]             ----->>>>>  Output: 5
+
+// Brute Force Approach
+// Time Complexity -> O(nlogn);
+// Space Complexity -> O(n);
+function secondLargest(arr) {
+  const uniqueArr = Array.from(new Set(arr));
+
+  uniqueArr.sort((a, b) => {
+    return b - a;
+  });
+
+  if (uniqueArr.length >= 2) {
+    return uniqueArr[1];
+  } else {
+    return -1;
+  }
+}
+
+// Optimised Approach
+// Time Complexity -> O(n);
+// Space Complexity -> O(1);
+function secondLargestOptimised(arr) {
+  let largest = Number.NEGATIVE_INFINITY;
+  let secondLargest = Number.NEGATIVE_INFINITY;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > largest) {
+      secondLargest = largest;
+      largest = arr[i];
+    } else if (arr[i] != largest && arr[i] > secondLargest) {
+      secondLargest = arr[i];
+    }
+  }
+  return secondLargest;
+}
+
+console.log(secondLargestOptimised([10, 5, 10]));
